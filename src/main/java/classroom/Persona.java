6b8347275192d89aab709b8879b5dbc31121de85
fmunoze <fmunoze@unal.edu.co>
2022-08-31 11:41:19 -0500

    V1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,17 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+
+    Persona() {
+        this.nombre = "Sin nombre";
+        this.cedula = 0;
     }
 
     Persona(long cedula, String nombre) {
@@ -31,6 +35,7 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
     }
     
